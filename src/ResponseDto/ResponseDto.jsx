2279fe4a5a65ae64f3b88abd46369915abdd67cc
CodeGitJae <CodeGitJae@gmail.com>
2024-09-24 22:39:55 +0900

    db에서 뽑아온 데이터 출력 확인

--- src/ResponseDto/ResponseDto.jsx
+++ src/ResponseDto/ResponseDto.jsx
@@ -17,13 +17,14 @@ const ResponseAPI  = () => {
     return (
         <div>
             백엔드에서 가져온 특기코드 : 
-            {data.map(item => (
+            {hello.map(item => (
                 <div key={item.id}>
-                    <p>{item.teukgiCode}</p>
-                    <p>{item.teukgiCodeStr}</p>
-                    <p>{item.gunGubun}</p>
+                    <p>{item.id}: 특기코드: {item.teukgiCode || item.teukgiCodeStr}</p>
+                    <p>군사구분: {item.gunGubun}</p>
+                    <hr />
                 </div>
             ))}
+
         </div>
     );
 };
